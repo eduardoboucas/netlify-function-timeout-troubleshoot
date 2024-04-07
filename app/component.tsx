29bd241bca65b340adb8566948eaa3e23ced7486
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-04-08 01:04:08 +0530

    added query duration

--- app/component.tsx
+++ app/component.tsx
@@ -1,6 +1,8 @@
 'use client'
 import {gql, useSuspenseQuery} from '@apollo/client'
+import {performance} from 'node:perf_hooks'
 export default function() {
+  const start = performance.now()
   const countries = useSuspenseQuery<{
     countries : Array<{
       name : string
@@ -20,5 +22,6 @@ export default function() {
       }
     }
   })
+  console.log('query duration:', performance.now() - start)
   return <div>THIS IS A SUSPENSE TEST: {countries.data.countries[0].name}</div>
 }
